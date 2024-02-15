5093884e94868aacf3320cc0e34e4d0962cfcdd1
Luca Pette <ciao@lucapette.me>
2024-02-15 10:53:05 +0100

    fix: mount k8s deployment so the server can find it (#100)
    I'm not very happy about mounting in a place and have the server copy
    the files into the system config place. But I honestly can't find a
    simple way to mount configs into a container (in a r/w directory). As
    this may just be my ignorance, it may also change soon

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencies {
     implementation(libs.test.containers.redpanda)
     implementation(libs.bundles.sf4j)
+    implementation(libs.mockk)
     implementation("net.peanuuutz.tomlkt:tomlkt:0.3.7")
-
-    testImplementation(libs.mockk)
 }
