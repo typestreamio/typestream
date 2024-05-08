ba35cfb29109abb0cd8289c04c1ad63c4eda0ca5
lucapette <ciao@lucapette.me>
2024-05-08 22:21:37 +0200

    build: update kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    implementation("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:1.9.24")
     implementation("com.adarshr:gradle-test-logger-plugin:4.0.0")
     implementation("org.jetbrains.kotlinx:kover:0.6.1")
 }
