4febfba2e2cf07fd4323846a4df5b1bf3f7afa16
Jevin Maltais <jevin@quickjack.ca>
2025-11-24 06:48:13 -0500

    Basic flake setup (#117)

--- flake.nix
+++ flake.nix
@@ -2,9 +2,15 @@
   # Adapted from: https://github.com/the-nix-way/dev-templates/blob/main/kotlin/flake.nix
   description = "A Nix-flake-based Kotlin development environment";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
     let
       javaVersion = 20;
       # Some overlay to inject your dependencies (pkgs.jdk, pkgs.gradle, pkgs.kotlin) should be defined
@@ -15,31 +21,48 @@
           kotlin = prev.kotlin.override { jre = jdk; };
         })
       ];
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        # https://ryantm.github.io/nixpkgs/builders/special/fhs-environments/#sec-fhs-environments
-        # WARNING! As I understand, with this configuration your shell will be running in a namespace.
-        # It isolates the filesystem (or at least a part of the filesystem) to behave as a standard
-        # FHS system. gradlew should work normally, thus handling all your dependencies by itself.
-        # With this configuration, your only required dependency is the jdk17.
-        # Thus it is not a packaging flake, but rather a flake to enable development for your tool.
-        default = (pkgs.buildFHSUserEnv {
-          name = "java-dev-env";
-          targetPkgs = pkgs:
-            with pkgs; [
-              jdk17
-              # Gradle for building containers
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = overlays;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          # https://ryantm.github.io/nixpkgs/builders/special/fhs-environments/#sec-fhs-environments
+          # WARNING! As I understand, with this configuration your shell will be running in a namespace.
+          # It isolates the filesystem (or at least a part of the filesystem) to behave as a standard
+          # FHS system. gradlew should work normally, thus handling all your dependencies by itself.
+          # With this configuration, your only required dependency is the jdk17.
+          # Thus it is not a packaging flake, but rather a flake to enable development for your tool.
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              jdk
               gradle
-              # For the cli
+              kotlin
               docker-compose
+              go
+              gcc
+              pkg-config
+              minikube
+              bash
             ];
-          runScript = "bash";
-        }).env;
-      });
+          };
+        }
+      );
     };
 }
