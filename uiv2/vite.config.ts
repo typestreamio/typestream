6ce0b9acff273f31533353a36c570270795cc6f6
Jevin Maltais <jevin@quickjack.ca>
2026-01-09 15:09:02 -0500

    Don't kill the CPU by watching EVERYTHING

--- uiv2/vite.config.ts
+++ uiv2/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: false,
+      ignored: ['**/.pnpm-store/**'],
+    },
+  },
 })
