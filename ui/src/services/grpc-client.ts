a2bbd7d16d8abb5096fc0969c07b86cec54e8573
Jevin Maltais <jevin@quickjack.ca>
2026-02-06 12:18:58 -0500

    chore: improve Renovate config and remove deprecated ui/ folder
    - Add explicit dockerfile fileMatch for uiv2/Dockerfile to detect Node and Caddy base images
    - Add regex manager for Elasticsearch Docker images in docker-compose files
    - Remove deprecated ui/ folder (fully replaced by uiv2/)

--- ui/src/services/grpc-client.ts
+++
@@ -1,7 +0,0 @@
-import { GrpcWebImpl, FileSystemServiceClientImpl } from '../generated/filesystem';
-
-const transport = new GrpcWebImpl('http://localhost:8080', {
-  debug: true,
-});
-
-export const fileSystemClient = new FileSystemServiceClientImpl(transport);
