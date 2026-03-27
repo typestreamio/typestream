088e9120fbdc4e905867221bbe7b3a6c0b611ee0
Jevin Maltais <jevin@quickjack.ca>
2026-03-27 11:50:04 -0400

    chore(deps): update dependency org.jetbrains.kotlinx:kover-gradle-plugin to v0.9.8 (#274)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,5 +11,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     implementation("org.jetbrains.kotlin:kotlin-serialization:2.3.0")
     implementation("com.adarshr:gradle-test-logger-plugin:4.0.0")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.9.4")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.9.8")
 }
